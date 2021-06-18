aebc9db1678d5e6e72810b5967ad6812f816ae83
Haley Parish <heparish@ucdavis.edu>
2021-06-18 13:14:08 -0700

    test

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ const app = express();
 // allows info to go to app and vice versa
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/db'));
+app.use(express.static(__dirname, '/public'));
 // app.use(htmlRoutes)
 
 require('./routes/apiroutes')(app);
